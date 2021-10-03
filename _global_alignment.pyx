d86ab8332ebd670af3591ba46ad495b513aecfa6
Hideto Mori <morityun@sfc.keio.ac.jp>
2021-10-04 01:16:53 +0900

    Modified traceback

--- _global_alignment.pyx
+++ _global_alignment.pyx
@@ -1,6 +1,7 @@
 import numpy as np 
 cimport numpy as np
 cimport cython
+from cpython cimport array
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -16,35 +17,32 @@ def align(char *x, char *y, int match, int mis_match, int gap_extension, int gap
     cdef int k
     cdef int l 
     cdef int v
+    cdef int s 
+    cdef int t 
+    cdef int n
     cdef int pre
     cdef int maxv
     cdef int[:,:] M
     cdef int[:,:] P
     cdef int[:,:] Q
-    cdef int[:,:] S
-    cdef int[:,:] T
-
-    lx = len(x) 
-    ly = len(y) 
+    cdef array.array[char] arrx, arry, template = array.array('b')
+    lx  = len(x) 
+    ly  = len(y)
+    arrx = array.clone(template, lx + ly, False)
+    arry = array.clone(template, lx + ly, False)
     M = np.zeros((lx+1, ly+1), dtype=np.int32)
     P = np.zeros((lx+1, ly+1), dtype=np.int32)
     Q = np.zeros((lx+1, ly+1), dtype=np.int32)
-    S = np.zeros((lx+1, ly+1), dtype=np.int32)
-    T = np.zeros((lx+1, ly+1), dtype=np.int32)
 
     for i in range(1, lx+1):
         M[i,0] = -65535
         P[i,0] = -65535
-        S[i,0] = -65535
         Q[i,0] = end_weight * (gap_start + i * gap_extension)
-        #T[i,0] = 2
     
     for i in range(1, ly+1):
         M[0,i] = -65535
         P[0,i] = end_weight * (gap_start + i * gap_extension) 
         Q[0,i] = -65535
-        S[0,i] = -65535
-        #T[0,i] = 1
 
     for i in range(1, lx+1):
         for j in range(1, ly+1):
@@ -65,51 +63,73 @@ def align(char *x, char *y, int match, int mis_match, int gap_extension, int gap
                     gap_extension + gap_start + P[i-1,j],
                     gap_extension + Q[i-1,j]
             )
-            S[i,j] = -65535
     
-    S[lx,ly] = max(M[lx,ly], P[lx,ly], Q[lx,ly])
-    for i in range(0, lx+1):
-        k = lx - i
-        for j in range(0, ly+1): 
-            l = ly - j
-            if S[k,l] == -65535:
-                pass 
+    
+    k = lx
+    l = ly
+    s = max(M[lx,ly], P[lx,ly], Q[lx,ly])
+    
+    if M[k,l] == s:
+        t = 1
+    elif P[k,l] == s:
+        t = 2
+    else:
+        t = 3
+
+    n = 0
+    while k > 0 or l > 0:
+        if s == M[k,l] and t == 1:
+            if x[k-1] == y[l-1]:
+                v = match 
             else:
-                if S[k,l] == M[k,l] and (T[k,l] == 1 or (i==0 and j ==0)):
-                    if x[k-1] == y[l-1]:
-                        v = match 
-                    else:
-                        v = mis_match  
-                    if v + M[k-1,l-1] == S[k,l]:
-                        S[k-1,l-1] = M[k-1,l-1] 
-                        T[k-1,l-1] = 1
-                    elif v + P[k-1,l-1] == S[k,l]:
-                        S[k-1,l-1] = P[k-1,l-1]
-                        T[k-1,l-1] = 2
-                    elif v + Q[k-1,l-1] == S[k,l]:
-                        S[k-1,l-1] = Q[k-1,l-1]
-                        T[k-1,l-1] = 3
+                v = mis_match  
+            if v + M[k-1,l-1] == s:
+                s = M[k-1,l-1] 
+                t = 1
+            elif v + P[k-1,l-1] == s:
+                s = P[k-1,l-1]
+                t = 2
+            elif v + Q[k-1,l-1] == s:
+                s = Q[k-1,l-1]
+                t = 3
+            
+            arrx[n] = x[k-1]
+            arry[n] = y[l-1]
+            k = k - 1
+            l = l - 1
 
-                elif S[k,l] == P[k,l] and (T[k,l] == 2 or (i==0 and j ==0)):
-                    if gap_extension + gap_start + M[k,l-1] == S[k,l]:
-                        S[k,l-1] = M[k,l-1]
-                        T[k,l-1] = 1
-                    elif gap_extension + P[k,l-1] == S[k,l] or k == 0:
-                        S[k,l-1] = P[k,l-1]
-                        T[k,l-1] = 2
-                    elif gap_extension + gap_start + Q[k,l-1] == S[k,l]:
-                        S[k,l-1] = Q[k,l-1]
-                        T[k,l-1] = 3
+        elif s == P[k,l] and t == 2:
+            if gap_extension + gap_start + M[k,l-1] == s:
+                s = M[k,l-1]
+                t = 1
+            elif gap_extension + P[k,l-1] == s or k == 0:
+                s = P[k,l-1]
+                t = 2
+            elif gap_extension + gap_start + Q[k,l-1] == s:
+                s = Q[k,l-1]
+                t = 3
+            
+            arrx[n] = "-"
+            arry[n] = y[l-1]
+            k = k
+            l = l - 1
 
-                elif S[k,l] == Q[k,l] and (T[k,l] == 3 or (i==0 and j ==0)):
-                    if gap_extension + gap_start + M[k-1,l] == S[k,l]:
-                        S[k-1,l] = M[k-1,l]
-                        T[k-1,l] = 1
-                    elif gap_extension + gap_start + P[k-1,l] == S[k,l]:
-                        S[k-1,l] = P[k-1,l]
-                        T[k-1,l] = 2
-                    elif gap_extension + Q[k-1,l] == S[k,l] or l == 0:
-                        S[k-1,l] = Q[k-1,l]
-                        T[k-1,l] = 3 
-
-    return M, P, Q, S
+        elif s == Q[k,l] and t == 3:
+            if gap_extension + gap_start + M[k-1,l] == s:
+                s = M[k-1,l]
+                t = 1
+            elif gap_extension + gap_start + P[k-1,l] == s:
+                s = P[k-1,l]
+                t = 2
+            elif gap_extension + Q[k-1,l] == s or l == 0:
+                s = Q[k-1,l]
+                t = 3 
+            
+            arrx[n] = x[k-1]
+            arry[n] = "-"
+            k = k - 1
+            l = l
+        n += 1
+    arrx = arrx[:n][::-1]
+    arry = arry[:n][::-1]
+    return M, P, Q, arrx, arry
--- global_alignment.py
+++ global_alignment.py
@@ -1,53 +1,9 @@
 import _global_alignment as gal
 import numpy as np 
 
-def trace_single(matrix, a, b, seq):
-    while 1:
-        if matrix[a-1,b-1] != -65535:
-            seq += "."
-            a -= 1
-            b -= 1
-        
-        elif matrix[a,b-1] != -65535: 
-            seq += "-"
-            b -= 1
-        
-        elif matrix[a-1,b] != -65535: 
-            seq += "|"
-            a -= 1
-        
-        if a == 0 and b == 0:
-            break 
-
-    return seq
-
-def traceback(matrix, x, y):
-    seq      = ""
-    p,q      = len(x), len(y)    
-    seq = trace_single(matrix, p, q, seq)
-    new_x = ""
-    new_y = ""
-    xi = 0 
-    yi = 0
-    for char in seq[::-1]:
-        if char == ".":
-            new_x += x[xi]
-            new_y += y[yi] 
-            xi += 1
-            yi += 1
-        elif char == "|":
-            new_x += x[xi] 
-            new_y += "-"
-            xi += 1
-        elif char == "-":
-            new_x += "-"
-            new_y += y[yi]
-            yi += 1
-    return new_x, new_y 
-
 def global_alignment(subject, query, match=10, mis_match=-1, gap_extension=-1, gap_open=-20): 
     result = gal.align(subject.encode('utf-8'),query.encode('utf-8'), match, mis_match, gap_extension, gap_open, 1)
-    a, b   = traceback(result[3], subject, query)  
+    a, b   = "".join(map(chr, result[3])), "".join(map(chr, result[4]))
     return a,b 
 
 if __name__ == "__main__":
@@ -56,7 +12,6 @@ if __name__ == "__main__":
     from tqdm import tqdm
     from Bio import pairwise2
     from Bio.pairwise2 import format_alignment
-
     def read_fastq(fastq_name):
         #Read fastq file
         seq_dict = collections.defaultdict(dict)
@@ -77,22 +32,23 @@ if __name__ == "__main__":
                 n += 1
         return seq_dict 
 
-    path = "./"
+    path = "../../sample_data/Target-ACE_sequence_data/Rep1/AID/polyC/chr19_8211972/"
     ref  = "TAACTTACGGAGTCGCTCTACGCTAAGTCCCCGTGTAAACAGAGCTGAACCTGCAGGCAGGTAAGAGTGTCCCCGGCCTGTGCCCCCCCACCTCCAGACGGCGGTAGCACTCACGTACGACACACTGGCTCCTGCCAGGCAGGACCTAAAGAATCCCATCC" 
     fastq_dict = read_fastq(path + "/R1.fastq")
     
     with open("result.txt", "w") as o:
-        for key in fastq_dict:
+        for key in tqdm(fastq_dict):
             query = fastq_dict[key]["seq"] 
             a, b  = global_alignment(ref, query, match=10, mis_match=-1, gap_extension=-1, gap_open=-20) 
-            print(">", file=o)
-            print("reference_sequence", a, sep=",", file=o)
-            print(key, b, sep=",", file=o)
+            #print(a)
+            #print(b) 
+            #print(">", file=o)
+            #print("reference_sequence", a, sep=",", file=o)
+            #print(key, b, sep=",", file=o)
 
     """
     x = "ATGAGTCTCTCTGATAAGGACAAGGCTGCTGTGAAAGCCCTATGG"
     y = "CTGTCTCCTGCCGACAAGACCAACGTCAAGGCCGCCTGGGGTAAG"
-
     #x = "ATGAGTCTCTCTGATAAGGACAAGGCTG"
     #y = "CTGTCTCCTGCCGACAAGACCAACGTCA"
     #x = "AAAA"
@@ -101,36 +57,7 @@ if __name__ == "__main__":
     #y = "ATTTTGC"
     #y = "ACACT"
     #x = "AAT" 
-    result = gal.align(x.encode('utf-8'), y.encode('utf-8'), 1, -1, -1, -3, 1)
-    for row in result[0]:
-        new_row = []
-        for n in row:
-            new_row.append(int(n))
-        print(*new_row, sep="\t") 
-    print()
-    
-    for row in result[1]:
-        new_row = []
-        for n in row:
-            new_row.append(int(n))
-        print(*new_row, sep="\t") 
-    print()
-    
-    for row in result[2]:
-        new_row = []
-        for n in row:
-            new_row.append(int(n))
-        print(*new_row, sep="\t") 
-    print()
-    
-    for row in result[3]:
-        new_row = []
-        for n in row:
-            new_row.append(int(n))
-        print(*new_row, sep="\t") 
-    print()
-
-    a, b = traceback(result[3],x,y)
+    a, b = global_alignment(x, y, match=1, mis_match=-1, gap_extension=-1, gap_open=-3) 
     print(a)
     print(b)
     print()
